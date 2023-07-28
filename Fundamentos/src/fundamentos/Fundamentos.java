bc41171560a95a9fe17221bcb362c1b3b5b43601
Murilo Ignez <muriloignez27@gmail.com>
2023-07-28 11:06:27 -0300

    Entendendo os Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -10,9 +10,13 @@ public class Fundamentos {
         String nome = "Murilo";
         int idade = 40;
         boolean estudando = true;
-        double media = 9;
+        double media = 9.5;
         
         String bairro = new String("Parque Orlanda III");
+        //Declaração de Wrappers
+        int nota1 = Integer.parseInt("9");
+        double mediaf = Double.parseDouble("9.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
