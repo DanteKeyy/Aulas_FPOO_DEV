2633db5c3082b9f3e36db84416b4c2d7653a08af
Murilo Ignez <muriloignez27@gmail.com>
2023-07-28 10:49:22 -0300

    criando variaveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package fundamentos;
 
-/**
- *
- * @author Aluno
- */
+
 public class Fundamentos {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaração de variaveis
+        String nome = "Murilo";
+        int idade = 40;
+        boolean estudando = true;
+        double media = 9;
     }
     
 }
